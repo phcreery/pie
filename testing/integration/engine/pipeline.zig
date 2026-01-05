e069f34593b1983ba0ec449d88da75e81f7addd1
phcreery <phcreery@gmail.com>
2026-01-04 19:51:25 -0600

    save

--- testing/integration/engine/pipeline.zig
+++ testing/integration/engine/pipeline.zig
@@ -40,16 +40,15 @@ fn runPipeBench(allocator: std.mem.Allocator, pipeline: *Pipeline) !void {
 
 pub fn traverseDAG(allocator: std.mem.Allocator, pipeline: *Pipeline, node_pool: *pie.pipeline.NodePool) !void {
     // Map from `id` to `mark` value
-    var mark = std.AutoHashMap(usize, u8).init(allocator);
+    var mark = std.AutoHashMap(pie.pipeline.NodeHandle, u8).init(allocator);
     defer mark.deinit();
 
-    var stack: [1000]usize = undefined; // Stack to hold node IDs
+    var stack: [1000]pie.pipeline.NodeHandle = undefined; // Stack to hold node IDs
     var sp: isize = -1; // Stack pointer
 
     var node_pool_handles = node_pool.liveHandles();
     while (node_pool_handles.next()) |node_handle| {
-        // const dst_node = node_pool.getPtr(node_handle) catch unreachable;
-        try mark.put(node_handle.id, 0);
+        try mark.put(node_handle, 0);
     }
 
     // Initialize stack with all nodes that have no dependencies (sink nodes)
@@ -60,8 +59,8 @@ pub fn traverseDAG(allocator: std.mem.Allocator, pipeline: *Pipeline, node_pool:
             if (socket) |sock| {
                 if (sock.type == .sink) {
                     sp += 1;
-                    stack[@as(usize, @intCast(sp))] = node_handle.id;
-                    try mark.put(node_handle.id, 1); // Mark as in-progress
+                    stack[@as(usize, @intCast(sp))] = node_handle;
+                    try mark.put(node_handle, 1); // Mark as in-progress
                     break;
                 }
             }
@@ -69,34 +68,31 @@ pub fn traverseDAG(allocator: std.mem.Allocator, pipeline: *Pipeline, node_pool:
     }
 
     while (sp >= 0) {
-        const currId = stack[@as(usize, @intCast(sp))];
-        // const currNode = findNode(nodes, currId) catch return;
-        const currNode = node_pool.getPtr(pie.pipeline.NodeHandle{ .id = currId }) catch return;
-
-        const currentMark = mark.getPtr(currId) orelse return;
-        if (currentMark.* == 1) {
+        const curr_handle = stack[@as(usize, @intCast(sp))];
+        const curr_node = node_pool.getPtr(curr_handle) catch return;
+        const curr_mark = mark.getPtr(curr_handle) orelse return;
+        if (curr_mark.* == 1) {
             // First time processing this node, push its children onto the stack
-            try mark.put(currId, 2); // Pre-visit handling (mark as in-progress)
-            for (currNode.desc.sockets) |child_socket| {
+            try mark.put(curr_handle, 2); // Pre-visit handling (mark as in-progress)
+            for (curr_node.desc.sockets) |child_socket| {
                 const socket = child_socket orelse continue;
-                // const connected_to_node = socket.private.connected_to_node orelse continue;
                 const connected_to_node = pipeline.getConnectedNode(socket) orelse continue;
-                const child_id = connected_to_node.item.id;
-                const childMark = mark.getPtr(child_id) orelse return;
-                if (childMark.* == 0) { // If child is unvisited
+                const child_node = connected_to_node.item;
+                const child_mark = mark.getPtr(child_node) orelse return;
+                if (child_mark.* == 0) { // If child is unvisited
                     sp += 1;
                     if (sp >= stack.len) {
                         return error.StackOverflow;
                     }
-                    stack[@as(usize, @intCast(sp))] = child_id;
-                    try mark.put(child_id, 1); // Mark as in-progress
+                    stack[@as(usize, @intCast(sp))] = child_node;
+                    try mark.put(child_node, 1); // Mark as in-progress
                 }
             }
         } else {
             // All children have been processed, post-visit handling
-            try mark.put(currId, 3); // Mark as finished
+            try mark.put(curr_handle, 3); // Mark as finished
             // Process currNode here (e.g., print or store in result list)
-            std.debug.print("Visited node ID: {d}\n", .{currId});
+            std.debug.print("Visited node: {any}\n", .{curr_handle});
             sp -= 1; // Pop the current node off the stack
         }
     }
